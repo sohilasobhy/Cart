001f2ba8e18049607921cc8f0da04015bb51472e
Admin <sohilasobhysadek@gmail.com>
2025-01-14 23:32:15 +0200

    New updates

---
+++ Index.js
@@ -0,0 +1,8 @@
+document.getElementById('fullscreenToggle').addEventListener('click', function () {
+    const modalDialog = document.getElementById('modalDialog');
+    modalDialog.classList.toggle('modal-fullscreen');
+});
+document.getElementById('fullscreenToggle2').addEventListener('click', function () {
+    const modalDialog = document.getElementById('modalDialog2');
+    modalDialog.classList.toggle('modal-fullscreen');
+});
