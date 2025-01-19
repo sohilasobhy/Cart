3ebfc4f55479569e18df736585f05578dcf5d121
Admin <sohilasobhysadek@gmail.com>
2025-01-19 18:23:33 +0200

    Final 19/1/2025

--- Index.js
+++ Index.js
@@ -6,3 +6,11 @@ document.getElementById('fullscreenToggle2').addEventListener('click', function 
     const modalDialog = document.getElementById('modalDialog2');
     modalDialog.classList.toggle('modal-fullscreen');
 });
+document.getElementById("btn-close1").addEventListener('click', function () {
+    const modalDialog = document.getElementById('modalDialog');
+    modalDialog.classList.remove('modal-fullscreen');
+})
+document.getElementById("btn-close2").addEventListener('click', function () {
+    const modalDialog = document.getElementById('modalDialog2');
+    modalDialog.classList.remove('modal-fullscreen');
+})
